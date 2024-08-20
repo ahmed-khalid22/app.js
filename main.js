5d7b3f9dd3c5a4f37d3b962933c2763bd6d69bf0
ahmed-khalid22 <ahmedkhkh2004@gmail.com>
2024-08-20 13:31:31 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,96 @@
+// document.querySelector("h1").style.color='blue';
+// window.alert("hello js");
+// console.log("hello ahmed");
+// console.table(["ahmed","waleed","khalid"]);
+console.log("%chello %cahmed","color:red; font-size:20px","color:blue; font-size:20px");
+// var myname = "ahmed";
+// console.log(typeof("hello "+myname));
+// console.log(typeof 5000);
+// document.querySelector("h1").style.color='blue';
+// console.log("%cel zero %cweb %cschool","color:red; font-size:40px", "color:green; font-size:40px", "color:blue; font-size:40px" );
+// console.table(["EL zero", "ahmed","sameh","waleed","aya"] );
+// console.log("Iam In Console");
+// document.write("Iam In Page");
+// var user = "ahmed";
+// console.log(user);
+/*console.log("elzero");
+console.log('elzero');
+console.log("'elzero'");
+console.log('"elzero"');
+console.log("elzero web \"school\"");
+console.log("elzero \\ web school");
+console.log("elzero \nweb \nschool");
+console.log("elzero \
+     web \
+      school");*/
+// let a = "ahmed";
+// let b = "khalid";
+// document.write(a + " " + b);
+
+// let a = "we love";
+// let b = "javascript";
+// let c = "and";
+// let d = "programming";
+// console.log(a + " " +b + " " +c + " " +d);
+/*let x = 30;
+console.log(x);
+
+function hellox(){
+      let x =50;
+      console.log(x);
+}
+hellox();
+console.log(x);
+
+var y = 20;
+console.log(y);
+
+function helloy(){
+      var y = 40;
+      console.log(y);
+}
+helloy();
+console.log(y);*/
+
+
+
+
+let x = "ahmed ";
+let y = 7;
+let user = {name:"ahmed", age:21, date:"19/8/2024"};
+let students = [10 ,20 , 30 , "ahmed"];
+let newStudents = new Array(50, 60, 70, 80);
+y = y + students[1];
+
+console.log(x);
+console.log(y);
+console.log(user);
+console.log(user.name);
+console.log(students[2]);
+console.log(newStudents);
+
+console.log(typeof(x));
+console.log(typeof(y));
+console.log(typeof(user));
+console.log(typeof(user.name));
+console.log(typeof(students));
+console.log(typeof(newStudents));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
